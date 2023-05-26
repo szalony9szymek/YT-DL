ab98c1673faf68f085018c9c6b4d133171421b30
szalony9szymek <szymon.p06@gmail.com>
2023-05-26 12:52:15 +0200

    make project

---
+++ Form1.cs
@@ -0,0 +1,17 @@
+namespace YT_DL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1.FormBorderStyle = FormBorderStyle.FixedSingle;
+            Form1.MaximizeBox = false;
+            Form1.MinimizeBox = false;
+        }
+    }
+}
